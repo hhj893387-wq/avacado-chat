f44476dca3fb43ac4845378b0c8e834cc21b78ea
hhj893387-wq <hhj893387@gmail.com>
2026-07-11 01:59:37 -0700

    Update OneSignalSDKUpdaterWorker.js

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,2 @@
 /* OneSignal SDK Updater Worker - Required for keeping OneSignal SDK updated */
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
